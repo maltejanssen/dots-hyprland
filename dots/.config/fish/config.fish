cf4e8e9e26b097c33d73d65d5ace3c1da15570a1
Malte Janssen <mjan@ngc.dk>
2026-01-27 12:45:04 +0100

    feat: set gpg_tty env var

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -11,6 +11,8 @@ if status is-interactive # Commands to run in interactive sessions can go here
     # No greeting
     set fish_greeting
 
+    set -gx GPG_TTY (tty)
+
     # Use starship
     starship init fish | source
     if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
